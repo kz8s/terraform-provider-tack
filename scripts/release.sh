fc15f33c89ef4cf7f76db210c3004c3ccf9f322f
jono wells <_@oj.io>
2016-05-14 11:46:47 -0700

    added - tgz packaging in `make release` goal

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -eux
 
 FN=terraform-provider-tack
+VER=$(git describe --dirty)
 
 buildOsArch() {
   GOOS=$1
   GOARCH=$2
   mkdir -p release/$GOOS/$GOARCH
   go build -o release/$GOOS/$GOARCH/$FN
+
+  tar pczf release/${FN}_${VER}_${GOOS}-${GOARCH}.tgz -C release/$GOOS/$GOARCH $FN
 }
 
 buildOsArch darwin amd64
