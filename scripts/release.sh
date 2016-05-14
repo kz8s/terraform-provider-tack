0cb7e5f23c36f78199eaea8b2f4e16d8f7301fb8
jono wells <_@oj.io>
2016-05-14 13:17:06 -0700

    added - sha256 checksum generation of release artifacts

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,8 +9,11 @@ buildOsArch() {
   mkdir -p release/$GOOS/$GOARCH
   go build -o release/$GOOS/$GOARCH/$FN
 
-  tar pczf release/${FN}_${VER}_${GOOS}-${GOARCH}.tgz -C release/$GOOS/$GOARCH $FN
+  tar pczf release/${FN}_${VER}_${GOOS}-${GOARCH}.tar.gz -C release/$GOOS/$GOARCH $FN
+  rm -rf release/$GOOS
 }
 
 buildOsArch darwin amd64
 buildOsArch linux amd64
+
+cd release && shasum -a 256 *.tar.gz > terraform-provider-tack_${VER}.SHA256SUMS
